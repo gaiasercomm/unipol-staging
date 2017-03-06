8b7a2c068a3584bed518d2eb877320bf71ad62a8
Terry Chen <terry_chen@sercomm.com>
2017-03-06 18:12:18 +0800

    1. Update version to 3.1.13g 2. modify CSRF

--- portal/filters.php
+++ portal/filters.php
@@ -35,7 +35,7 @@ App::before(function($request)
     // define("_CLASS", _ROOT . "/inc/class/");
 
     #*********
-    define("_PORTAL_VER", "3.1.13d");
+    define("_PORTAL_VER", "3.1.13g");
 
     #*********
     define("_COOKIEDOMAIN", "http://neutral_web/");
@@ -143,7 +143,7 @@ Route::filter('guest', function()
 
 Route::filter('csrf', function()
 {
-    if (Session::token() != Input::get('_token'))
+    if (Session::token() != Request::header('X-CSRF-TOKEN'))
     {
         throw new Illuminate\Session\TokenMismatchException;
     }
